5ba9ea236c4898d5f2c59883a32ac1c657c4898e
Dekeypetey40 <kmichaelmikhail@gmail.com>
2023-07-15 18:42:17 +0200

    Add event listener for element buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,50 @@
+let playerNumber;
+let cpuNumber;
 /**
  * Wait for the DOM to load
  * Hide the Reset Button until the game starts
  * Event listeners for the buttons
+ * Waits for element choice to use run
  */
-
 document.addEventListener("DOMContentLoaded", function() {
     //Hide the restart button
     document.getElementById("restart").style.display = "none";
     //start button event listener
     //Hides the start button, shows restart
+    //Calls the cpuChoice() function
     let startButton = document.getElementById("begin-duel");
     startButton.addEventListener("click", function() {
         document.getElementById("result").innerText = "Choose your element and your fate will be decided";
         document.getElementById("begin-duel").style.display = "none";
         document.getElementById("restart").style.display = "";
 
+
         })
+    //Listens for element button to be clicked 
+    //and assigns that element to player
+    let elementButtons = document.getElementsByClassName("element");
+    for (let element of elementButtons){
+        element.addEventListener("click", function() {
+        let playerElement = this.getAttribute("data-type"); 
+        cpuChoice();
+        randomDiceRoll();
+        
+
+        }
+        )}
+
+    
     //Inspired by code from stackoverflow
     let restartButton = document.getElementById("restart");
     restartButton.addEventListener("click", function() {
         location.reload();
     });
+
+    //Listen for player clicking element button
+
 });
 
-/* 
+
 
 function runDuel() {
 
@@ -34,10 +55,16 @@ function playerChoice() {
 function cpuChoice() {
     
 }
-/**Creates two random numbers between 1-20 */
+/**
+ * A random number between 1-20 is picked for both player
+ * and computer and is stored in variable
+ * before any bonuses are added.
+ */
 function randomDiceRoll() {
     let playerNumber = Math.floor(Math.random() * 20) + 1;
     let cpuNumber = Math.floor(Math.random() * 20) + 1;
+    console.log(playerNumber);
+    console.log(cpuNumber);
     
 }
 function compareElements() {
@@ -56,8 +83,5 @@ function displayResult() {
     
 }
 function scoreTally() {
-    
-}
-function restartGame() {
     
 }
