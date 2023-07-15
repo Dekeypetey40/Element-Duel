23ac21c4677dd392b2a0d7220f6c22ad40b6bccf
Dekeypetey40 <kmichaelmikhail@gmail.com>
2023-07-15 15:00:43 +0200

    Test click button event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
+// Wait for the DOM to load
+// Event listeners to the buttons
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") ==="submit") {
+                alert("You clicked Begin Duel");
+            } else {
+            let elementType = this.getAttribute("data-type");
+            alert(`You clicked the ${elementType} element`);
+            }
+        })
+    }
+
+})
 
 function runDuel() {
 
