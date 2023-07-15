c3ea633e0695e6f5d7d92f038143f0d24d3a9924
Dekeypetey40 <kmichaelmikhail@gmail.com>
2023-07-15 15:46:32 +0200

    Add event listener for begin duel button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,16 @@
 // Wait for the DOM to load
-// Event listeners to the buttons
+// Event listeners to the start button
 document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
+    let startButton = document.getElementById("begin-duel");
 
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") ==="submit") {
-                alert("You clicked Begin Duel");
-            } else {
-            let elementType = this.getAttribute("data-type");
-            alert(`You clicked the ${elementType} element`);
-            }
+    startButton.addEventListener("click", function() {
+        document.getElementById("result").innerText = "Choose your element and your fate will be decided";
+
         })
-    }
 
-})
+});
+
+/* 
 
 function runDuel() {
 
@@ -25,10 +21,19 @@ function playerChoice() {
 function cpuChoice() {
     
 }
+/**Creates two random numbers between 1-20 */
 function randomDiceRoll() {
+    let playerNumber = Math.floor(Math.random() * 20) + 1;
+    let cpuNumber = Math.floor(Math.random() * 20) + 1;
     
 }
 function compareElements() {
+    const elements = {
+        fire: "nature",
+        water: "fire",
+        nature: "water"
+    }
+
     
 }
 function chooseWinner() {
