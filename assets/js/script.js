82e5a5eee3ce482dd05665d714ecf6cddec3f74b
Dekeypetey40 <kmichaelmikhail@gmail.com>
2023-07-15 14:39:42 +0200

    Add: name all functions needed in js file

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+
+
+function runDuel() {
+
+}
+function playerChoice() {
+    
+}
+function cpuChoice() {
+    
+}
+function randomDiceRoll() {
+    
+}
+function compareElements() {
+    
+}
+function chooseWinner() {
+    
+}
+function displayResult() {
+    
+}
+function scoreTally() {
+    
+}
+function restartGame() {
+    
+}
